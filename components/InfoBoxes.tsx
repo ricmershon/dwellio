e130ca66031158e9f72fae151bcb3a55f4a5176c
Ric Mershon <rmersh@ricmershon.com>
2025-06-13 13:58:50 -0400

    home page info boxes

--- components/InfoBoxes.tsx
+++ components/InfoBoxes.tsx
@@ -1,7 +1,34 @@
-const InfoBoxes = () => {
-    return (
-        <div>InfoBoxes</div>
-    );
-}
+import InfoBox from "./InfoBox";
+
+const InfoBoxes = () => (
+    <section>
+        <div className="container-xl lg:container m-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+                <InfoBox
+                    headingText="For Renters"
+                    backgroundColor="bg-gray-100"
+                    buttonInfo={{
+                        link: "/properties",
+                        styles: "bg-black hover:bg-gray-700",
+                        text: "Browse Properties"
+                    }}
+                >
+                    Find your dream rental property. Bookmark properties and contact owners.
+                </InfoBox>
+                <InfoBox
+                    headingText="For Property Owners"
+                    backgroundColor="bg-blue-100"
+                    buttonInfo={{
+                        link: "/properties/add",
+                        styles: "bg-blue-500 hover:bg-blue-600",
+                        text: "Add Property"
+                    }}
+                >
+                    Find your dream rental property. Bookmark properties and contact owners.
+                </InfoBox>
+            </div>
+        </div>
+    </section>
+);
  
 export default InfoBoxes;
