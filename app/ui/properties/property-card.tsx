e388fd68b95e7909549bf4238af4c506d2c3e486
Ric Mershon <rmersh@ricmershon.com>
2025-07-25 13:16:57 -0400

    make entire proerty card, except favorite button, clickable

--- app/ui/properties/property-card.tsx
+++ app/ui/properties/property-card.tsx
@@ -20,6 +20,12 @@ const PropertyCard = async ({ property }: { property: PropertyDocument }) => {
 
     return (
         <div className='rounded-md shadow-md relative'>
+            {/* Don't display favorite button if user logged in and not current user */}
+            {sessionUser && sessionUser.id !== property.owner.toString() && (
+                <PropertyCardFavoriteButton
+                    propertyId={serializedProperty._id}
+                />
+            )}
             <Link href={`/properties/${property._id}`}>
                 <Image
                     src={property.imagesData![0].secureUrl}
@@ -29,47 +35,39 @@ const PropertyCard = async ({ property }: { property: PropertyDocument }) => {
                     sizes='100vw'
                     className='w-full h-auto rounded-t-md'
                 />
+                <div className='p-4 text-gray-500 text-xs md:text-sm'>
+                    <h3 className='absolute top-[10px] left-[10px] bg-white px-1 py-[2px] rounded-md text-gray-500 text-left md:text-center lg:text-right'>
+                        {getRateDisplay(property.rates)}
+                    </h3>
+                    <div className='flex justify-center gap-2 mb-2'>
+                        <p>
+                            {property.beds}{' '}
+                            <span><FaBed className='inline' /></span>
+                            
+                        </p>
+                        <p>
+                            {property.baths}{' '}
+                            <span><FaBath className='inline' /></span>
+                        </p>
+                                            <p>
+                            {property.squareFeet}{' '}
+                            <span><FaRulerCombined className='inline' /></span>
+                        </p>
+
+                    </div>
+                    <div className='flex justify-center gap-4'>
+                        <p>
+                            <FaMoneyBill className='text-green-900 inline' /> Weekly
+                        </p>
+                        <p>
+                            <FaMoneyBill className='text-green-900 inline' /> Monthly
+                        </p>
+                    </div>
+
+                    <div className='border border-gray-100 my-4'></div>
+                    <p className='font-semibold text-gray-700 text-center'>{property.type} in {property.location.city}</p>
+                </div>
             </Link>
-            <div className='p-4 text-gray-500 text-xs md:text-sm'>
-                <h3 className='absolute top-[10px] left-[10px] bg-white px-1 py-[2px] rounded-md text-gray-500 text-left md:text-center lg:text-right'>
-                    {getRateDisplay(property.rates)}
-                </h3>
-                
-                {/* Don't display favorite button if user logged in and not current user */}
-                {sessionUser && sessionUser.id !== property.owner.toString() && (
-                    <PropertyCardFavoriteButton
-                        propertyId={serializedProperty._id}
-                    />
-                )}
-                
-                <div className='flex justify-center gap-2 mb-2'>
-                    <p>
-                        {property.beds}{' '}
-                        <span><FaBed className='inline' /></span>
-                        
-                    </p>
-                    <p>
-                        {property.baths}{' '}
-                        <span><FaBath className='inline' /></span>
-                    </p>
-                                        <p>
-                        {property.squareFeet}{' '}
-                        <span><FaRulerCombined className='inline' /></span>
-                    </p>
-
-                </div>
-                <div className='flex justify-center gap-4'>
-                    <p>
-                        <FaMoneyBill className='text-green-900 inline' /> Weekly
-                    </p>
-                    <p>
-                        <FaMoneyBill className='text-green-900 inline' /> Monthly
-                    </p>
-                </div>
-
-                <div className='border border-gray-100 my-4'></div>
-                <p className='font-semibold text-gray-700 text-center'>{property.type} in {property.location.city}</p>
-            </div>
         </div>
     );
 };
