a8a1510edfe8d84788f73f1d600e4477611c7d5f
Ric Mershon <rmersh@ricmershon.com>
2025-07-31 17:57:35 -0400

    misc

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,6 +8,7 @@ import Breadcrumbs from "@/app/ui/shared/breadcrumbs";
 const PropertyPage = async ( { params }: { params: Promise<{ id: string }> }) => {
     const { id } = await params;
     const propertyDoc = await fetchProperty(id);
+    
     const property: PropertyDocument = JSON.parse(JSON.stringify(propertyDoc));
 
     return (
