c91f29186612c050f6999834328c2752ae925052
Ric Mershon <rmersh@ricmershon.com>
2025-08-13 12:30:54 -0400

    show unread message count if not mobile, otherwise show empty badge

--- ui/messages/unread-message-count.tsx
+++ ui/messages/unread-message-count.tsx
@@ -1,9 +1,12 @@
-const UnreadMessageCount = ( { unreadCount }: { unreadCount: number }) => {
-    return (
-        <span className="absolute top-1 right-1 inline-flex items-center justify-center size-[13px] text-[10px] leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
-            {unreadCount}
-        </span>
-    );
+interface UnreadMessageCountProps {
+    unreadCount: number;
+    viewportWidth: number;
 }
+
+const UnreadMessageCount = ({ unreadCount, viewportWidth }: UnreadMessageCountProps) => (
+    <span className="absolute top-1 right-1 inline-flex items-center justify-center size-[15px] md:size-[14px] text-[10px] leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
+        {viewportWidth > 640 && unreadCount}
+    </span>
+);
  
 export default UnreadMessageCount;
