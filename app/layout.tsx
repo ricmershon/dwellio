504bf1310f8bb882494cb4b0ef04c702bd81fc4f
Ric Mershon <rmersh@ricmershon.com>
2025-09-10 15:02:04 -0400

    root layout stub

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,12 @@ export const metadata: Metadata = {
   description: "Find your perfect property rental",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children, }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
